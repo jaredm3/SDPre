77501ef7491ed2d7a5c44b5f9f62fc9106d4b7e2
Jared M <mcgowanjs@gmail.com>
2019-08-18 12:23:16 -0500

    Exercise2 jthw

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt {
+
+   public static void main( String[] args ) {
+   System.out.println( "+------------------------+" );
+   System.out.println( "|                        |" );
+   System.out.println( "|      CORNER STORE      |" );
+   System.out.println( "|                        |" );
+   System.out.println( "| 2019-08-18  04:38PM    |" );
+   System.out.println( "|                        |" );
+   System.out.println( "| Gallons:       10.870  |" );
+   System.out.println( "| Price/gallon  $ 2.569  |" );
+   System.out.println( "|                        |" );
+   System.out.println( "| Fuel total:   $ 27.93  |" );
+   System.out.println( "|                        |" );
+   System.out.println( "+------------------------+" );
+  }
+
+}
