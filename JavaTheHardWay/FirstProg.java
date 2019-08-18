3ce4527b3689b062ccfdc26cc49118e17144f1bb
Jared M <mcgowanjs@gmail.com>
2019-08-18 12:08:16 -0500

    Exercise1 jthw

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,4 +1,5 @@
 public class FirstProg {
+  
   public static void main(String[] args) {
     System.out.println("I, Jared McGowan, am determined to learn code");
     System.out.println("Today's date is August 18, 2019");
