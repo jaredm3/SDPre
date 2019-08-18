20fb1840a667f3d21fc363c809af46cdc1173e1c
Jared M <mcgowanjs@gmail.com>
2019-08-18 13:31:32 -0500

    complete objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3{
+
+  public static void main(String[] args) {
+    System.out.println("Name : Jared McGowan");
+    System.out.println("Age : 28");
+    System.out.println("State : Nebraska");
+  }
+
+}
