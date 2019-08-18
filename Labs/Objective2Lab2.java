792ce10c9fb93360f8aa9224b1bc8174f29a3f87
Jared M <mcgowanjs@gmail.com>
2019-08-18 16:04:40 -0500

    Completed objective2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jared McGowan";
+    int age = 28;
+    String state = "Nebraska";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,14 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 6;
+    num2 = 35;
+    num3 = 98;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println("This output would result from numbers " + num1 + "," + num2 + "," + num3);
+    System.out.println(average);
+  }
+}
