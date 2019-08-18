d2cba3cf6f07695a5066196a602661239580d246
Jared M <mcgowanjs@gmail.com>
2019-08-18 13:00:46 -0500

    Exercise4 jthw

---
+++ JavaTheHardWay/PrintingChoicesEscapes.java
@@ -0,0 +1,9 @@
+public class PrintingChoicesEscapes {
+
+   public static void main( String[] args ) {
+   System.out.println( "Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\n"
+   + "Hotel\nIndia\n\nThis is a test.\nI am learning Java the Hard Way!" );
+
+  }
+
+}
