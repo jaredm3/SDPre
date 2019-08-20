e0a4e8dbde798f7b211649e6f7c7f2cfa928204f
Jared M <mcgowanjs@gmail.com>
2019-08-20 17:50:31 -0500

    Complete Objective6lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int sumTotal = 0;
+    int counter = 0;
+
+    while(counter <= 20) {
+      sumTotal += counter;
+      counter++;
+    }
+    System.out.println(sumTotal);
+  }
+}
